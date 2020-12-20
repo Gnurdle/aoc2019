a45a511af845ffc077b8e18cbce1d265f17c6bd4
Clay Hopperdietzel <chopper@cms-aero.com>
2020-12-20 14:20:22 -0600

    tweak ws for calva testing

--- src/main/aoc2019/core.cljs
+++ src/main/aoc2019/core.cljs
@@ -1,9 +1,20 @@
 (ns aoc2019.core
-  (:require [aoc2019.day1 :as day1]
+  (:require [cljs.core.async :refer-macros [go] :as A]
+            [aoc2019.day1 :as day1]
             [aoc2019.day2 :as day2]
-            [aoc2019.day3 :as day3]))
+            [aoc2019.day3 :as day3]
+            ["ws" :as ws]))
+
+;; start a websocket server, just to hang a resource out
+;; there to test lifecycle stuff
+(defn- setup-wss []
+  (let [wss (ws/Server. (clj->js {:port 8088}))]
+    (println "started websocket server @ 8088")
+    (.on wss "connection" #(println "got ws connection"))))
 
 (defn main []
+  (setup-wss)
+
   #_(cljs.repl/repl (cljs.repl.node/repl-env :port 8192))
   (let [runs
         [["day1a" day1/day1a 3382136]
@@ -12,15 +23,17 @@
          ["day2b" day2/day2b 8298]
          ["day3a" day3/day3a 1285]
          ]]
-    (doseq [[txt fnc expected] runs]
-      (let [start    (system-time)
-            result   (fnc)
-            stop     (system-time)
-            timestr  (str " " (.toFixed (- stop start) 6) " msecs")]
-        (if (= expected result)
-          (println txt "passed=" result timestr)
-          (println txt "failed, expected: " expected ", got: " result timestr))))))
+    (go
+      (doseq [[txt fnc expected] runs]
+        (let [start    (system-time)
+              result   (fnc)
+              stop     (system-time)
+              timestr  (str " " (.toFixed (- stop start) 6) " msecs")]
+          (if (= expected result)
+            (println txt "passed=" result timestr)
+            (println txt "failed, expected: " expected ", got: " result timestr)))))))
 
 (comment
   (main)
+  (day3/day3a)
   )
